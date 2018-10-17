5171ba944ebb854437e1d3e9234f7980ec586a5d
醉红尘 <2249361730@qq.com>
2018-10-17 17:21:26 +0800

    修改

--- idea-git-test/src/HelloGit.java
+++ idea-git-test/src/HelloGit.java
@@ -8,4 +8,10 @@ public class HelloGit {
         System.out.println("hello git!");
         System.out.println("hello git!");
     }
+
+    public void test(){
+        int i=0;
+        i += 6;
+        System.out.println(i);
+    }
 }
